ad52e51b7e132154f6fbfb7f4bcfe3011afb29a1
ilove028 <maoxuheng@cnpc.com.cn>
2023-05-23 16:04:59 +0800

    feat: use vao

--- lib/triangle/gl.ts
+++ lib/triangle/gl.ts
@@ -10,21 +10,24 @@ const main = (selector: string) => {
     // Init
     const program = createProgram(context, vertex, frag);
     const buffer = context.createBuffer();
-
+    const vao = context.createVertexArray();
+    context.bindVertexArray(vao);
     context.bindBuffer(context.ARRAY_BUFFER, buffer);
     context.bufferData(context.ARRAY_BUFFER, vertices, context.STATIC_DRAW);
     context.enableVertexAttribArray(0);
     context.vertexAttribPointer(0, 3, context.FLOAT, false, 3 * vertices.BYTES_PER_ELEMENT, 0);
-    context.bindBuffer(context.ARRAY_BUFFER, null);
+    // context.bindBuffer(context.ARRAY_BUFFER, null);
+    context.bindVertexArray(null);
 
     const render = () => {
       context.clearColor(0, 0, 0, 1);
       context.clear(context.COLOR_BUFFER_BIT);
       context.viewport(0, 0, size.width, size.height);
       context.useProgram(program);
-      context.bindBuffer(context.ARRAY_BUFFER, buffer);
-      context.enableVertexAttribArray(0);
-      context.vertexAttribPointer(0, 3, context.FLOAT, false, 3 * vertices.BYTES_PER_ELEMENT, 0);
+      // context.bindBuffer(context.ARRAY_BUFFER, buffer);
+      // context.enableVertexAttribArray(0);
+      // context.vertexAttribPointer(0, 3, context.FLOAT, false, 3 * vertices.BYTES_PER_ELEMENT, 0);
+      context.bindVertexArray(vao);
       context.drawArrays(context.TRIANGLES, 0, 3);
       requestAnimationFrame(render);
     }

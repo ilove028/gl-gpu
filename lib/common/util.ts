3ea535571c1820789db9876c5551c5ead2406898
ilove028 <maoxuheng@cnpc.com.cn>
2023-05-26 14:37:51 +0800

    chore: 初步增加

--- lib/common/util.ts
+++ lib/common/util.ts
@@ -63,7 +63,36 @@ const addKeyBoardControl = (pos: [number, number, number] = [0, 0, 2], tar: [num
   return () => viewMatrix;
 }
 
+const addMouseControl = (canvas: HTMLCanvasElement, pos: [number, number, number] = [0, 0, 2], tar: [number, number, number] = [0, 0, 0], u: [number, number, number] = [0, 1, 0]) => {
+  const position = vec3.fromValues(...pos);
+  const target = vec3.fromValues(...tar);
+  const up = vec3.normalize(vec3.create(), vec3.fromValues(...u));
+  const viewMatrix = mat4.create();
+  const computeViewMatrix = () => mat4.lookAt(viewMatrix, position, target, up);
+
+  canvas.addEventListener("mousedown", (e0: MouseEvent) => {
+    const { screenX: screenX0, screenY: screenY0 } = e0;
+    const direction = vec3.create();
+
+    vec3.normalize(direction, vec3.subtract(direction, target, position));
+    const handleMouseMove = (e1: MouseEvent) => {
+      const { target, screenX: screenX1, screenY: screenY1 } = e1;
+
+      if (target === canvas) {
+        const offsetX = screenX1 - screenX0;
+        const offsetY = screenY1 - screenY0;
+
+        
+      }
+    }
+    document.addEventListener("mousemove", handleMouseMove);
+  })
+
+  return () => viewMatrix
+}
+
 export {
   loadImage,
-  addKeyBoardControl
+  addKeyBoardControl,
+  addMouseControl
 }
